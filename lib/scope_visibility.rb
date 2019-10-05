2eb3a4f63fd0c234dc2e091c4eebc8bcfbb95c6e
meganemura <meganemura@users.noreply.github.com>
2019-10-05 12:09:22 +0900

    Implement ScopeVisibility::VisibilityMethods

--- lib/scope_visibility.rb
+++ lib/scope_visibility.rb
@@ -1,6 +1,48 @@
+require "binding_ninja"
 require "scope_visibility/version"
 
 module ScopeVisibility
-  class Error < StandardError; end
-  # Your code goes here...
+  module VisibilityMethods
+    def self.extended(klass)
+      klass.singleton_class.instance_eval do
+        extend(BindingNinja)
+        auto_inject_binding(:scope_visibility)
+        auto_inject_binding(:scope_visibility_public?)
+        auto_inject_binding(:scope_visibility_private?)
+        auto_inject_binding(:scope_visibility_protected?)
+      end
+    end
+
+    def scope_visibility(bind, binding_from_predicate_method = nil)
+      bind = binding_from_predicate_method if binding_from_predicate_method
+
+      method_name = "_scope_visibility_trial".to_sym
+
+      eval("def #{method_name}; end", bind, __FILE__, __LINE__ + 1)
+
+      visibility = if private_instance_methods(false).include?(method_name)
+                     :private
+                   elsif protected_instance_methods(false).include?(method_name)
+                     :protected
+                   else
+                     :public
+                   end
+
+      undef_method method_name
+
+      visibility
+    end
+
+    def scope_visibility_public?(bind)
+      scope_visibility(bind) == :public
+    end
+
+    def scope_visibility_private?(bind)
+      scope_visibility(bind) == :private
+    end
+
+    def scope_visibility_protected?(bind)
+      scope_visibility(bind) == :protected
+    end
+  end
 end
--- spec/scope_visibility_spec.rb
+++ spec/scope_visibility_spec.rb
@@ -1,9 +1,43 @@
 RSpec.describe ScopeVisibility do
-  it "has a version number" do
-    expect(ScopeVisibility::VERSION).not_to be nil
+  class Foo
+    extend ScopeVisibility::VisibilityMethods
+
+    @public_scope1 = scope_visibility_public?
+    @public_scope_visibility1 = scope_visibility
+
+    private
+
+    @private_scope1 = scope_visibility_private?
+    @private_scope_visibility1 = scope_visibility
+
+    protected
+
+    @protected_scope1 = scope_visibility_protected?
+    @protected_scope_visibility1 = scope_visibility
+
+    public
+
+    @public_scope2 = scope_visibility_public?
+    @public_scope_visibility2 = scope_visibility
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:klass) { Foo }
+
+  describe ".scope_visibility" do
+    it "returns scope visibility" do
+      expect(klass.instance_variable_get(:@public_scope_visibility1)).to eq(:public)
+      expect(klass.instance_variable_get(:@private_scope_visibility1)).to eq(:private)
+      expect(klass.instance_variable_get(:@protected_scope_visibility1)).to eq(:protected)
+      expect(klass.instance_variable_get(:@public_scope_visibility2)).to eq(:public)
+    end
+  end
+
+  describe ".scope_visibility_xxx? predicates" do
+    it "returns true" do
+      expect(klass.instance_variable_get(:@public_scope1)).to eq(true)
+      expect(klass.instance_variable_get(:@private_scope1)).to eq(true)
+      expect(klass.instance_variable_get(:@protected_scope1)).to eq(true)
+      expect(klass.instance_variable_get(:@public_scope2)).to eq(true)
+    end
   end
 end
